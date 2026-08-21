f5eea8cd2463c24c4fac2b48ec1fe440bbca2263
Raimundo Nonato Pereira da Silva <60166010+nonato3009@users.noreply.github.com>
2026-08-21 19:21:10 +0000

    adicionando arquivos .gitigonre, pages e `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Minha primeira página feita com o NEXT e REACT, MUITO IMPORTANTE</h1>
+  return (
+    <h1>Minha primeira página feita com o NEXT e REACT, MUITO IMPORTANTE</h1>
+  );
 }
 
 export default Home;
